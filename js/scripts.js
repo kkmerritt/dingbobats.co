f55bcba94c06949ec413c21abcb2300be329def6
kev merritt <kkmerritt@gmail.com>
2023-03-09 22:06:27 -0500

    first version of dingobats.co

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+/*!
+* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+console.log('accessed the /js/scripts.js');
+
+var loginInfo = "A8D8D788A0370E4B5B4649ACBA5E2BAD7079";
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
---
+++ readme.md
@@ -0,0 +1,31 @@
+#DEBUGGERS
+python3 -m http.server
+
+
+
+[project name] - kev_merritt
+December , 2022
+dingobats.co
+
+#PRODUCED ON:
+lenova ideapad 5: Linux, Ubuntu 20
+sitting: on Tone Fitness 65 cm exercise ball
+drinking: CuP on Norman Coffee, Bells Two-Hearted Ale, Sorrow.
+eating: marshmallows from swiss miss hot cocoa packets.
+
+# CREDITS:
+
+
+
+# GIT HUB
+[GitHub KKMERRITT](https://github.com/kkmerritt/nopro2)
+# HEROKU
+[HEROKU](https://HEROKU.COM)
+
+# TECHNICAL CHALLENGES:
+
+
+# TECHNICAL ASPECTS / FEATURES / FUNCTIONALITY
+
+
+# FUTURE IMPROVEMENTS:
